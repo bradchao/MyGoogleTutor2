25416914c84b2e5bf60f436b5201beafd4eec6f8
brad <mybradtw@gmail.com>
2019-04-09 13:30:31 +0800

    201904091330

---
+++ GTutor2/src/tw/brad/gtest2/Brad18.java
@@ -0,0 +1,19 @@
+package tw.brad.gtest2;
+
+public class Brad18 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		String[] mesg = {"OK", "XX"};
+		System.out.println(mesg[a]);
+		a = a ^ 1;
+		System.out.println(mesg[a]);
+		a = a ^ 1;
+		System.out.println(mesg[a]);
+		a = a ^ 1;
+		System.out.println(mesg[a]);
+		
+
+	}
+
+}
