091403135a1106708d90acca13d503790e681bd1
brad <mybradtw@gmail.com>
2019-04-16 14:52:09 +0800

    201904161452

---
+++ GTutor2/src/tw/brad/gtest2/Brad27.java
@@ -0,0 +1,15 @@
+package tw.brad.gtest2;
+
+public class Brad27 {
+
+	public static void main(String[] args) {
+		String id = "B123460300";
+		
+		if (TWId.isRightId(id)) {
+			System.out.println("OK");
+		}else {
+			System.out.println("XX");
+		}
+	}
+
+}
