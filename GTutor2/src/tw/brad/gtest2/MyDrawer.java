0c81f1a1800895934b4a4c43696c76ad8de8fd26
brad <mybradtw@gmail.com>
2019-05-14 13:20:56 +0800

    2019051403

--- GTutor2/src/tw/brad/gtest2/MyDrawer.java
+++ GTutor2/src/tw/brad/gtest2/MyDrawer.java
@@ -1,6 +1,9 @@
 package tw.brad.gtest2;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -9,4 +12,21 @@ public class MyDrawer extends JPanel {
 	public MyDrawer() {
 		setBackground(Color.GREEN);
 	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+//		System.out.println(g.getClass().getName());
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setColor(Color.BLUE);
+		g2d.setStroke(new BasicStroke(4));
+		g2d.drawLine(0, 0, 400, 400);
+
+		
+		
+		
+	}
+	
 }
