74b2e7a9c9e910d599d389fbcd66438673f389ed
brad <mybradtw@gmail.com>
2019-05-16 12:16:26 +0800

    201905161216

--- GTutor2/src/tw/brad/gtest2/Racing.java
+++ GTutor2/src/tw/brad/gtest2/Racing.java
@@ -1,6 +1,8 @@
 package tw.brad.gtest2;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +11,7 @@ import javax.swing.JLabel;
 public class Racing extends JFrame {
 	private JButton go;
 	private JLabel[] lanes = new JLabel[8];
+	private Car[] cars = new Car[8];
 	
 	public Racing() {
 		super("賽車");
@@ -16,17 +19,49 @@ public class Racing extends JFrame {
 		
 		go = new JButton("Go!");
 		add(go);
+		go.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				go();
+			}
+		});
 		
 		for (int i=0; i<lanes.length; i++) {
 			lanes[i] = new JLabel((i+1) + ". ");
 			add(lanes[i]);
 		}
 		
-		setSize(800, 480);
+		setSize(1024, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
+	
+	private void go() {
+		for (int i=0; i<cars.length; i++) {
+			cars[i] = new Car(i);
+			cars[i].start();
+		}
+		
+	}
+	
+	private class Car extends Thread {
+		private int lane;
+		Car(int lane){this.lane = lane;}
+		
+		@Override
+		public void run() {
+			for (int i=0; i<100; i++) {
+				lanes[lane].setText(lanes[lane].getText() + ">");
+				try {
+					Thread.sleep(100 + (int)(Math.random()*300));
+				}catch(Exception e) {
+					
+				}
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		new Racing();
