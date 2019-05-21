42f5b4298c08058288bf2169e6d8fe227a41df9b
brad <mybradtw@gmail.com>
2019-05-21 13:34:55 +0800

    201905211334

--- GTutor2/src/tw/brad/gtest2/Brad70.java
+++ GTutor2/src/tw/brad/gtest2/Brad70.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -25,6 +27,23 @@ public class Brad70 extends JFrame {
 	}
 	
 	private class MyPanel extends JPanel {
+		private Timer timer;
+		private int x, y;
+		
+		MyPanel(){
+			timer = new Timer();
+			
+			timer.schedule(new MyBallTask(), 1*1000, 200);
+		}
+		
+		private class MyBallTask extends TimerTask {
+			@Override
+			public void run() {
+				x += 10; y+= 10;
+				repaint();
+			}
+		}
+		
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
@@ -35,10 +54,7 @@ public class Brad70 extends JFrame {
 				BufferedImage ball = 
 						ImageIO.read(new File("imgs/ball.png"));
 				
-				g2d.drawImage(ball,
-		                100,
-		                100,
-		                null);
+				g2d.drawImage(ball,x,y,null);
 				
 			}catch(Exception e) {
 				System.out.println(e.toString());
